49145f6d6b0e55ea5674fbf1e03ef889ea6d0884
Tonya <elzeresdai@gmail.com>
2023-04-09 15:00:27 +0300

    added requests continued working with data objects

--- app/Services/WeArePentagon/WeArePentagonService.php
+++ app/Services/WeArePentagon/WeArePentagonService.php
@@ -3,20 +3,29 @@
 declare(strict_types=1);
 
 namespace App\Services\WeArePentagon;
+
 use App\Services\Concerns\BuildBaseRequest;
 use App\Services\Concerns\CanSendGetRequest;
+use App\Services\Concerns\CanSendPostRequest;
+use App\Services\Resources\ApiDataResource;
+
 
 class WeArePentagonService
 
 {
     use BuildBaseRequest;
     use CanSendGetRequest;
+    use CanSendPostRequest;
 
     public function __construct(
         private readonly string $baseUrl,
         private readonly string $apiToken,
     ) {}
 
-
-    
+    public function apiData(): ApiDataResource
+    {
+        return new ApiDataResource(
+            service: $this,
+        );
+    }
 }
